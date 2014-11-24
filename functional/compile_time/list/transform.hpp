215392f075a3c5e49b9a9106b14d0076eb2630a0
Globicodeur <guillaume.depardon@outlook.com>
2014-11-24 04:44:38 +0100

    Using my new static for in the compile time map

--- functional/compile_time/list/transform.hpp
+++ functional/compile_time/list/transform.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "tools/compile_time/static_for.hpp"
+
 namespace functional {
     namespace compile_time {
         namespace list {
@@ -13,47 +15,34 @@ namespace functional {
                     size_t n
                 >
                 constexpr auto operator()(const F & f, const C<T, n> & c) const {
+                    using tools::compile_time::for_;
                     using std::result_of;
                     using Tr = typename result_of<F(T)>::type;
 
                     C<Tr, n> r;
 
-                    map_impl<n - 1>(f, r, c);
+                    for_<0, n, map_impl>(f, r, c);
 
                     return r;
                 }
 
             private:
 
-                template <
-                    size_t i,
-                    class F,
-                    template <class, size_t> class C,
-                    class Tr,
-                    class T,
-                    size_t n,
-                    class = typename std::enable_if<i == 0, void>::type
-                >
-                constexpr void map_impl(const F & f, C<Tr, n> & r,
-                                        const C<T, n> & c) const {
-                    std::get<i>(r) = f(std::get<i>(c));
-                }
+                template <int i> struct map_impl {
 
-                template <
-                    size_t i,
-                    class F,
-                    template <class, size_t> class C,
-                    class Tr,
-                    class T,
-                    size_t n,
-                    class = typename std::enable_if<i != 0, void>::type,
-                    class = void
-                >
-                constexpr void map_impl(const F & f, C<Tr, n> & r,
-                                        const C<T, n> & c) const {
-                    std::get<i>(r) = f(std::get<i>(c));
-                    map_impl<i - 1>(f, r, c);
-                }
+                    template <
+                        class F,
+                        template <class, size_t> class C,
+                        class Tr,
+                        class T,
+                        size_t n
+                    >
+                    constexpr void operator()(const F & f, C<Tr, n> & r,
+                                              const C<T, n> & c) const {
+                        std::get<i>(r) = f(std::get<i>(c));
+                    }
+
+                };
 
             } map;
 
